a51f06a6fefcdf94741e41e4e1e24aa4fed7442d
haifishbro209 <kaloyannikolov016@gmail.com>
2025-08-02 13:18:34 +0200

    refactor bubble sort implementation for clarity and performance

--- sort_arrays/bubble_sort.py
+++ sort_arrays/bubble_sort.py
@@ -3,17 +3,18 @@ from create_array import *
 
 def sort(arr):
     length = len(arr)
-    for i in range(length):
-        a = arr[i]
-        if i < length - 1:
-            b = arr[i+1]
-        else:
-            break           
-        if a > b:
-            arr[i] = b
-            arr[i+1] = a
+    for j in range(length):
+        for i in range(length - j):
+            a = arr[i]
+            if i < length - 1:
+                b = arr[i+1]
+            else:
+                break           
+            if a > b:
+                arr[i] = b
+                arr[i+1] = a
     return arr
 
-arr = random_array(100)
+arr = random_array(100000)
 sorted = sort(arr)
 print(f'{sorted} \n \n \n {is_sorted(sorted)}')
