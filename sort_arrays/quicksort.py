d192717ec0921b07329ad216b71f3682acd51668
haifishbro209 <kaloyannikolov016@gmail.com>
2025-08-03 18:06:19 +0200

    refactor quicksort implementation for correctness and clarity

--- sort_arrays/quicksort.py
+++ sort_arrays/quicksort.py
@@ -1,14 +1,33 @@
 from create_array import random_array
 from random import randint
 
+
+test_array = [4,5,2,7,9,89]
+
+
 def quicksort(arr):
     length = len(arr)
-    split_point = arr[randint(0,length)]
+    position = randint(0,length)
+    split_point = arr[position]
+
     
-    for i in range(length):
-        
+    for i in range(position):
+        if arr[i] > split_point:
+            arr[position] = arr[i]
+            arr.pop(i)
+            position -= 1
 
+    for i in range(position ,length):
+        if arr[i] < split_point:
+            arr[position] = arr[i]
+            arr.pop(i)
+            position += 1
 
+    first_half = quicksort(arr[0:position])
+    second_half = quicksort(arr[position +1:length])
+    return arr
+    
 
+print(quicksort(test_array))
 
-arr = random_array(100)
+#arr = random_array(100)
