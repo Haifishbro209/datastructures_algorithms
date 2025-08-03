f759b0f1614746e4b96c29fa9da97aab604341a5
haifishbro209 <kaloyannikolov016@gmail.com>
2025-08-03 18:23:22 +0200

    REMOVING RANDOM pivot

--- sort_arrays/quicksort.py
+++ sort_arrays/quicksort.py
@@ -7,24 +7,33 @@ test_array = [4,5,2,7,9,89]
 
 def quicksort(arr):
     length = len(arr)
-    position = randint(0,length)
+    if length == 0: return arr
+    if length == 1:
+        position = 0
+    else:
+        position = randint(0,length -1)
+
     split_point = arr[position]
 
+
     
-    for i in range(position):
+    for i in range(position -1):
         if arr[i] > split_point:
             arr[position] = arr[i]
             arr.pop(i)
             position -= 1
 
-    for i in range(position ,length):
+    for i in range(position ,length -1):
         if arr[i] < split_point:
-            arr[position] = arr[i]
+            arr.insert(position,arr[i])
             arr.pop(i)
             position += 1
 
     first_half = quicksort(arr[0:position])
     second_half = quicksort(arr[position +1:length])
+    print(f'arr == {arr}')
+    print(f'first_half == {first_half}')
+    print(f'second_half == {second_half}')
     return arr
     
 
