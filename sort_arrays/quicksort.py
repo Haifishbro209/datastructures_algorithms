ef2c572dec6dbbda385034a89f1405f79067f11f
haifishbro209 <kaloyannikolov016@gmail.com>
2025-08-04 06:47:48 +0200

    almost works

--- sort_arrays/quicksort.py
+++ sort_arrays/quicksort.py
@@ -2,41 +2,30 @@ from create_array import random_array
 from random import randint
 
 
-test_array = [4,5,2,7,9,89]
+test_array = [4,5,2,7,9,89,12,6]
 
 
 def quicksort(arr):
     length = len(arr)
-    if length == 0: return arr
-    if length == 1:
-        position = 0
-    else:
-        position = randint(0,length -1)
-
-    split_point = arr[position]
-
-
+    pivot = arr[length-1]
+    swap = 0
+    i = -1
+    for j in range(length-1):
+        if arr[j] < pivot:
+            i += 1
+            swap = arr[i]
+            arr[i] = arr[j]
+            arr[j] = swap
     
-    for i in range(position -1):
-        if arr[i] > split_point:
-            arr[position] = arr[i]
-            arr.pop(i)
-            position -= 1
+    swap = arr[i+1]
+    arr[i+1] = pivot
+    arr[length-1] = swap
 
-    for i in range(position ,length -1):
-        if arr[i] < split_point:
-            arr.insert(position,arr[i])
-            arr.pop(i)
-            position += 1
-
-    first_half = quicksort(arr[0:position])
-    second_half = quicksort(arr[position +1:length])
-    print(f'arr == {arr}')
-    print(f'first_half == {first_half}')
-    print(f'second_half == {second_half}')
-    return arr
+    if length > 1:
+        first_part = quicksort(arr[0:i])
+        second_part = quicksort(arr[i + 2: length])
     
+    return first_part + [pivot] + second_part
 
 print(quicksort(test_array))
 
-#arr = random_array(100)
