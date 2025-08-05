e917af73df9bb4007721397af5fd6dfcf7ef6b5d
haifishbro209 <kaloyannikolov016@gmail.com>
2025-08-05 09:39:59 +0200

    doesn'T work mergesort

--- sort_arrays/merge_sort.py
+++ sort_arrays/merge_sort.py
@@ -3,4 +3,19 @@ arr = [7,0,45,5,93,11,6]
 
 def mergesort(arr):
     length = len(arr)
-    return arr
+    if length > 2:
+        mergesort(arr[0:int(length/2)])
+        mergesort(arr[int(length /2)])
+    else:
+        sorted = merge([arr[0]],[arr[1]])
+        return sorted
+    
+def merge(left,right):
+    arr = []
+    for i in left:
+        for j in right:
+            if i > j:
+                arr.extend(j)
+    
+
+print(mergesort(arr))
