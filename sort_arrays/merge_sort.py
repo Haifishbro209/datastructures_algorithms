ef69e5d2b816380e2697f1b885add76d75e4c095
haifishbro209 <kaloyannikolov016@gmail.com>
2025-08-05 19:45:34 +0200

    mergesort works

--- sort_arrays/merge_sort.py
+++ sort_arrays/merge_sort.py
@@ -5,48 +5,52 @@ def mergesort(arr):
     length = len(arr)
     if length < 2:
         return
-    middle = int(length /2)
+    middle = int(length / 2)
 
     leftarray = [0] * middle
     rightarray = [0] * (length - middle)
 
     for i in range(middle):
         leftarray[i] = arr[i]
-    
+
     j = 0
-    for i in range(middle +1, length -1):
+    for i in range(middle, length):
         rightarray[j] = arr[i]
         j += 1
 
     mergesort(leftarray)
-    mergesort(rightarray)    
-    merge(leftarray,rightarray,arr)
+    mergesort(rightarray)
+    merge(leftarray, rightarray, arr)
+
 
 def merge(left, right, array):
     left_size = len(left)
-    right_size = len(array) - left_size
+    right_size = len(right)
 
     i = 0
     r = 0
     l = 0
-    
+
     while l < left_size and r < right_size:
         if left[l] < right[r]:
             array[i] = left[l]
-            i+=1
-            l +=1
+            i += 1
+            l += 1
         else:
             array[i] = right[r]
-            i +=1
-            r +=1
-    
+            i += 1
+            r += 1
 
-    while l < left_size :
-        array[i]= left[l]
+    while l < left_size:
+        array[i] = left[l]
+        i += 1
+        l += 1
 
     while r < right_size:
-        array[i]= right[r]
-    
+        array[i] = right[r]
+        i += 1
+        r += 1
+
 print(array)
 
 mergesort(array)
